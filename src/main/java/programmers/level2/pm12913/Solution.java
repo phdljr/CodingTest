71b395e8dcde78e914170531a4f3137ba718b0c5
phdljr <phdljr@naver.com>
2023-07-24 03:09:10 +0900

    push

--- src/main/java/programmers/level2/pm12913/Solution.java
+++ src/main/java/programmers/level2/pm12913/Solution.java
@@ -2,17 +2,14 @@ package programmers.level2.pm12913;
 
 public class Solution {
     public int solution(int[][] land) {
-        int answer = 0;
 
-        return answer;
-    }
+        for(int i=1;i<land.length;i++){
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+        }
 
-    public static void main(String[] args) {
-        int[][] arr = {
-                {1,2,5,8},
-                {5,6,7,10},
-                {4,3,2,11}
-        };
-        System.out.println(new Solution().solution(arr));
+        return Math.max(land[land.length-1][0], Math.max(land[land.length-1][1], Math.max(land[land.length-1][2], land[land.length-1][3])));
     }
 }
