06e9a5c234ecad3dbc678ffd6fccfd17885296f3
phdljr <phdljr@naver.com>
2023-08-09 00:11:56 +0900

    push

--- src/main/java/programmers/level2/pm131704/Solution.java
+++ src/main/java/programmers/level2/pm131704/Solution.java
@@ -1,7 +1,56 @@
 package programmers.level2.pm131704;
 
+import java.util.*;
+
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("123".compareTo("124"));
+    public int solution(int[] order) {
+        int[] conv = new int[order.length];
+        for(int i=0;i<conv.length;i++){
+            conv[order[i]-1] = i+1;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int next = 1;
+        for(int i=0;i<conv.length;i++){
+            if(conv[i] == next){
+                next++;
+                while(!stack.isEmpty()){
+                    if(stack.peek() == next){
+                        stack.pop();
+                        next++;
+                    } else{
+                        break;
+                    }
+                }
+            } else{
+                if(stack.isEmpty()){
+                    stack.push(conv[i]);
+                }
+                else if(stack.peek() == next){
+                    while(!stack.isEmpty()){
+                        if(stack.peek() == next){
+                            stack.pop();
+                            next++;
+                        } else{
+                            break;
+                        }
+                    }
+                } else{
+                    stack.push(conv[i]);
+                }
+            }
+        }
+
+        while(!stack.empty()){
+            if(stack.peek() == next){
+                stack.pop();
+                next++;
+            } else{
+                break;
+            }
+        }
+
+        return next-1;
     }
 }
