76b83150ca2c00f50710cfdde9f7c018ede832cf
phdljr <phdljr@naver.com>
2023-08-24 00:10:19 +0900

    push

--- src/main/java/programmers/level2/pm12952no/Solution.java
+++ src/main/java/programmers/level2/pm12952no/Solution.java
@@ -3,27 +3,34 @@ package programmers.level2.pm12952no;
 public class Solution {
 
     static int answer = 0;
-    static int[] vx = new int[12]; // 인덱스: 퀸의 행, 값: 퀸의 열
+    static int[] vy = new int[13]; // 1~12
 
-    public int solution(int n){
-        go(0, n);
+    public int solution(int n) {
+        for(int i=1;i<=n;i++){
+            go(1, i, n);
+        }
         return answer;
     }
 
-    public void go(int depth, int n){
-        if(depth == n){
+    public void go(int x, int y, int n){
+        // 1. 행 위치 확인
+        // 2. 열 위치 확인
+        // 3. 대각선 위치 확인
+        for(int i=1;i<x;i++){
+            if(x == i) return;
+            if(y == vy[i]) return;
+            if(Math.abs(x - i) == Math.abs(y - vy[i])) return;
+        }
+
+        if(x == n){
             answer++;
             return;
         }
-        for(int i=0;i<n;i++){
-            vx[depth] = i;
 
-            for(int j=0;j<depth;j++){
-                if(vx[i] == vx[j]) return;
-                if(Math.abs(i - j) == Math.abs(vx[i] - vx[j])) return;
-            }
+        vy[x] = y; // x행의 y값 저장
 
-            go(depth+1, n);
+        for(int i=1;i<=n;i++){
+            go(x+1, i, n);
         }
     }
 
