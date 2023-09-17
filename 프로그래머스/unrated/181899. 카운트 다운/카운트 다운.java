22a770733a757ff313364ec3573bcbc535dc5452
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-09-18 01:12:43 +0900

    [unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        int j=0;
+        for(int i=start;i>=end_num;i--, j++){
+            answer[j] = i;
+        }
+        return answer;
+    }
+}
