59c4940dc3a803d74c209516ed6800370bcea743
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-10-11 15:31:16 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        set.add(a); set.add(b); set.add(c);
+        
+        if(set.size() == 3){
+            answer = a + b + c;
+        } else if(set.size() == 2){
+            answer = (a + b + c) * (a*a + b*b + c*c);
+        } else{
+            answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        }
+            
+        return answer;
+    }
+}
