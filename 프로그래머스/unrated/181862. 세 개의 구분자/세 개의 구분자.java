6921a349a0159a0d46fa90ad753cdd1ab038d2b4
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-10-16 03:29:20 +0900

    [level unrated] Title: 세 개의 구분자, Time: 49.52 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myStr) {
+        String[] answer = Arrays.stream(myStr.split("[abc]")).filter(s -> !s.equals("")).toArray(String[]::new);
+        return answer.length == 0 ? new String[]{"EMPTY"} : answer;
+    }
+}
