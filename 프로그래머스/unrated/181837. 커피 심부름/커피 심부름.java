3127fb8fbecb301769c8f9f815e95f6bae14e956
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-10-17 00:21:51 +0900

    [level unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String str: order){
+            if(str.contains("americano")){
+                answer += 4500;
+            } else if(str.contains("cafelatte")){
+                answer += 5000;
+            } else{
+                answer += 4500;
+            }
+        }
+        return answer;
+    }
+}
