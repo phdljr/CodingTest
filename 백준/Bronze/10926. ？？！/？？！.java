523b6ee5b58661d44d96206928997d51c3d4269f
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-11-15 10:08:09 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        out.write(st.nextToken()+"??!");
+        out.flush();
+    }
+}
