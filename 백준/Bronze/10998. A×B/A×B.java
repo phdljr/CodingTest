71727032c447eab3568bebd586f0662a67c4eacd
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-11-15 10:01:27 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] sarr = in.readLine().split(" ");
+        out.write(String.valueOf(Integer.parseInt(sarr[0]) * Integer.parseInt(sarr[1])));
+        out.flush();
+    }
+
+}
