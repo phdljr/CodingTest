ae2a52b0c27e9eb364cf9c7be4efbb57c67daff2
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-11-22 14:34:03 +0900

    [level 1] Title: 덧칠하기, Time: 2.52 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+        
+        for(int i=0;i<section.length;i++){
+            check[section[i]-1] = true;
+        }
+        
+        for(int i=0;i<check.length;i++){
+            if(check[i] == true){
+                for(int j=i;j<i+m && j < check.length;j++){
+                    check[j] = false;
+                }
+                answer++;
+            }
+        }   
+        
+        return answer;
+    }
+}
