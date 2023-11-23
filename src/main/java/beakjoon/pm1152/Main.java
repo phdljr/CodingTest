2ba0ed0f0ed1e9b6b3401850abe7c4cb23552479
phdljr <phdljr@naver.com>
2023-11-23 15:08:44 +0900

    push

---
+++ src/main/java/beakjoon/pm1152/Main.java
@@ -0,0 +1,20 @@
+package beakjoon.pm1152;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = in.readLine().trim().split(" ").length;
+
+        out.write(""+count);
+        out.flush();
+    }
+}
