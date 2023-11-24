6c3181b396e5097c9dec1befa8a3b2d371be3271
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-11-24 10:48:41 +0900

    [level 1] Title: 문자열 나누기, Time: 0.69 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char x = s.charAt(0);
+        int c1=0, c2=0;
+        int i = 0;
+        while(i < s.length()){
+            if(x == s.charAt(i)){
+                c1++;
+            } else{
+                c2++;
+            }
+            i++;
+            if(c1 == c2 && i < s.length()){
+                answer++;
+                x = s.charAt(i);
+            }
+            
+            if(i == s.length()){
+                answer++;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
