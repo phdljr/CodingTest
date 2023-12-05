8aa8388ce751e2d774983dd53fa30521824724ce
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-12-05 21:10:43 +0900

    [level 1] Title: 달리기 경주, Time: 0.05 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<players.length;i++){
+            map.put(players[i], i);
+        }
+        
+        for(int i=0;i<callings.length;i++){
+            int rank = map.get(callings[i]);
+            
+            String temp = players[rank - 1];
+            players[rank - 1] = players[rank];
+            players[rank] = temp;
+            
+            map.put(callings[i], rank - 1);
+            map.put(temp, rank);
+        }
+        
+        return players;
+    }
+}
