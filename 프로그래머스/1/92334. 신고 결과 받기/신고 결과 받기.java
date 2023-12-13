9b0ddc699943b2835673e83e849abfd2fec87341
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-12-13 22:06:56 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.16 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        HashMap<String, HashSet<String>> map = new HashMap<>();
+        boolean[] block = new boolean[id_list.length];
+        HashMap<String, Integer> email = new HashMap<>();
+
+        for(String id: id_list){
+            map.put(id, new HashSet<String>());
+            email.put(id, 0);
+        }
+
+        for(int i=0;i<report.length;i++){
+            String[] str = report[i].split(" ");
+            map.get(str[1]).add(str[0]);
+        }
+
+        for(int i=0;i<id_list.length;i++){
+            if(map.get(id_list[i]).size() >= k){
+                block[i] = true;
+            }
+            if(block[i]){
+                for(String reportId: map.get(id_list[i])){
+                    email.put(reportId, email.get(reportId)+1);
+                }
+            }
+        }
+
+        for(int i=0;i<email.size();i++){
+            answer[i] = email.get(id_list[i]);
+        }
+
+        return answer;
+    }
+}
