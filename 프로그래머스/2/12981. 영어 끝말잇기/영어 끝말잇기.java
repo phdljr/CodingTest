81d5796eaf53b4d4824e4dacc784052e7903665b
이종렬 <68233342+phdljr@users.noreply.github.com>
2023-12-16 14:00:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2]; // 사람, 몇 번째 단어
+
+        ArrayList usedWords = new ArrayList();
+        for(int i=0;i< words.length;i++){
+            String word = words[i];
+            if(usedWords.contains(word)){
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }
+
+            usedWords.add(word);
+
+            // 앞 뒤 체크
+            if(i == 0)
+                continue;
+            
+            String prev = words[i-1];
+            char c1 = prev.charAt(prev.length()-1);
+            char c2 = word.charAt(0);
+            if(c1 != c2){
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
