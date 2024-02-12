453937e259b5426522750de3d5f613b9348e2176
이종렬 <68233342+phdljr@users.noreply.github.com>
2024-02-12 23:03:17 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for(int num:num_list){
+            if(n == num)
+                return 1;
+        }
+        return 0;
+    }
+}
