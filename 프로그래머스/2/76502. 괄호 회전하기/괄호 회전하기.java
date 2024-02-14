28edf1100088430bc4f3e730e82550ed6200f19d
이종렬 <68233342+phdljr@users.noreply.github.com>
2024-02-14 21:48:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 10.00 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            s = s.substring(1, s.length()) + s.charAt(0);
+            Stack<Character> stack = new Stack<>();
+            for(int j=0;j<len;j++){
+                char ch = s.charAt(j);
+                if(!stack.empty()){
+                    char top = stack.peek();
+                    stack.push(ch);
+                    if(top == '(' && ch == ')'){
+                        stack.pop();
+                        stack.pop();
+                    } else if(top == '{' && ch == '}'){
+                        stack.pop();
+                        stack.pop();
+                    }else if(top == '[' && ch == ']'){
+                        stack.pop();
+                        stack.pop();
+                    }
+                } else{
+                    stack.push(ch);
+                }
+            }
+            if(stack.empty())
+                answer++;
+        }
+        return answer;
+    }
+}
