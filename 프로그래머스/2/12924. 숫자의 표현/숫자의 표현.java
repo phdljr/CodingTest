d6e115ce34cd433df03890cea6cc06c5ce6711eb
이종렬 <68233342+phdljr@users.noreply.github.com>
2024-02-16 22:45:24 +0900

    [level 2] Title: 숫자의 표현, Time: 1.51 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        int sum;
+        for(int i=n;i>0;i--){
+            sum = 0;
+            for(int j=i;j>0;j--){
+                sum+=j;
+                if(sum > n)
+                    break;
+                if(sum == n){
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
