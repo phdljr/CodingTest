f99ebd70597923916de1e2a65c8ee4f44458ed06
이종렬 <68233342+phdljr@users.noreply.github.com>
2024-02-29 18:31:45 +0900

    [level 2] Title: 롤케이크 자르기, Time: 16.88 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int[] topping) {
+        int[] left = new int[10001];
+        int[] right = new int[10001];
+        int r = 0; // 오른쪽의 토핑 개수
+        // 오른쪽의 토핑 개수를 먼저 세림
+        for (int x : topping) {
+            if (right[x] == 0) {
+                r++;
+            }
+            right[x]++;
+        }
+        int ans = 0;
+        int l = 0; // 왼쪽의 토핑 개수
+        // 왼쪽의 토핑을 하나씩 추가하며 오른족과 개수 비교
+        // 오른쪽의 토핑이 1에서 0이 되면 개수도 하나 감소
+        for (int x : topping) {
+            if (right[x] == 1) {
+                r--;
+            }
+
+            if (left[x] == 0) {
+                l++;
+            }
+            right[x]--;
+            left[x]++;
+
+            if (l == r) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
