2570fe454c483aabde07c2a4049c3a57401e35cd
이종렬 <68233342+phdljr@users.noreply.github.com>
2024-04-16 21:43:07 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i=0;i<emergency.length;i++){
+            int count = 1;
+            for(int j=0;j<emergency.length;j++){
+                if(i == j){
+                    continue;
+                }
+                
+                if(emergency[i] < emergency[j]){
+                    count++;
+                }
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
