239d29292bbacfc362dc1809c4b57395e0458266
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-11-29 14:08:27 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 476 ms, Memory: 45344 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder result = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        while(true){
+            stack.clear();
+            String str = br.readLine();
+
+            if(str.equals("."))
+                break;
+
+            String[] arr = str.replaceAll("[^()\\[\\]]", "").split("");
+
+            for(String ps: arr){
+                if(arr.length == 1 && arr[0].isEmpty())
+                    break;
+
+                stack.push(ps);
+
+                while(true){
+                    if(stack.isEmpty() || stack.size() < 2){
+                        break;
+                    }
+
+                    String top = stack.peek();
+                    String prev = stack.get(stack.size()-2);
+
+                    if(prev.equals("(") && top.equals(")") || prev.equals("[") && top.equals("]")){
+                        stack.pop();
+                        stack.pop();
+                    } else{
+                        break;
+                    }
+                }
+            }
+
+            if(stack.isEmpty())
+                result.append("yes");
+            else
+                result.append("no");
+            result.append("\n");
+        }
+
+        bw.write(result.toString().trim());
+        bw.flush();
+    }
+}
+
