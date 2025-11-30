046215f2a0b55d892e83b5c6b513645059b1fc48
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-11-30 20:17:08 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 124 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> list = new ArrayList<>();
+
+        String[] str =br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        for(int i=1;i<=n;i++){
+            list.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int index = k - 1;
+        while (true){
+            sb.append(list.remove(index)).append(" ");
+            if(list.isEmpty())
+                break;
+
+            index = (index + k - 1) % list.size();
+        }
+
+        String result = String.join(", ", sb.toString().split(" "));
+
+        bw.write("<"+ result + ">");
+        bw.flush();
+    }
+}
+
+
