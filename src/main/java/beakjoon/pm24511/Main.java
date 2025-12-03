f680865a063301e1672d95da2e1e249fee58feed
phdljr <phdljr@naver.com>
2025-12-03 17:32:11 +0900

    add

--- src/main/java/beakjoon/pm24511/Main.java
+++ src/main/java/beakjoon/pm24511/Main.java
@@ -6,6 +6,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Queue;
+
+enum Type{
+    QUEUE, STACK;
+}
+
+class Struct{
+    Type type;
+    int element;
+
+    public Struct(int type, int element) {
+        if(type == 0)
+            this.type = Type.QUEUE;
+        else
+            this.type = Type.STACK;
+        this.element = element;
+    }
+
+    public int pushAndPop(int input){
+        int result;
+        if(type == Type.QUEUE){
+            result = element;
+            element = input;
+        } else{
+            result = input;
+        }
+
+        return result;
+    }
+}
+
+class StructFactory{
+    Struct[] structs;
+
+    public StructFactory(int size, int[] types, int[] elements) {
+        this.structs = new Struct[size];
+
+        for(int i=0;i<size;i++){
+            structs[i] = new Struct(types[i], elements[i]);
+        }
+    }
+
+    public int pushAndPop(int input){
+        int result = input;
+        for(int i=0;i<structs.length;i++){
+            result = structs[i].pushAndPop(result);
+        }
+
+        return result;
+    }
+}
 
 public class Main {
 
@@ -16,12 +67,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] type = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StructFactory structFactory = new StructFactory(n, type, arr);
 
         n = Integer.parseInt(br.readLine());
         int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for(int input: inputs){
-
+            result.append(structFactory.pushAndPop(input)).append(" ");
         }
 
         bw.write(result.toString().trim());
