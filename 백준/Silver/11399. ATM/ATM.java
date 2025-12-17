e364e10d40e1b25d63c8b1bdc4d0c76fb233cb02
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-17 19:41:39 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(arr);
+
+        int result = 0;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            result += sum;
+        }
+
+        bw.write(result + "");
+        bw.flush();
+    }
+}
