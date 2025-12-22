6e652f96849e0b6e5ea127fb0740cbf3508e5a57
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-22 13:44:00 +0900

    [Silver III] Title: N번째 큰 수, Time: 848 ms, Memory: 256208 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,37 +1,37 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < N; i++) {
-
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-            for (int j = 0; j < N; j++) {
-                int num = Integer.parseInt(st.nextToken());
-
-                pq.add(num);
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] str = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(str[j]);
+                if (pq.size() == n) {
+                    if (pq.peek() < num) {
+                        pq.poll();
+                        pq.add(num);
+                    }
+                } else {
+                    pq.add(num);
+                }
             }
-
-        }
-        int size = pq.size() - N;
-
-        for (int i = 0; i < size; i++) {
-            pq.poll();
         }
 
-        System.out.println(pq.peek());
-
+        bw.write(pq.peek() + "");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
