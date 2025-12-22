dfee0bb863e8d4a34b512487f88984a80ae1695a
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-22 13:29:09 +0900

    [Silver IV] Title: 수 찾기, Time: 616 ms, Memory: 47564 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        br.readLine();
+        int[] find =  Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder result = new StringBuilder();
+        for(int f: find){
+            int i = Arrays.binarySearch(arr, f);
+            if(i <= -1){
+                result.append(0 + "\n");
+            } else{
+                result.append(1 + "\n");
+            }
+        }
+
+        bw.write(result.toString().trim());
+        bw.flush();
+    }
+}
