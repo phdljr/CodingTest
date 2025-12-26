ba7909ff083196052351390f426cfcf903cc2d8e
phdljr <phdljr@naver.com>
2025-12-27 02:46:13 +0900

    add

--- src/main/java/beakjoon/pm10986/Main.java
+++ src/main/java/beakjoon/pm10986/Main.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -18,26 +21,31 @@ public class Main {
 
         int m = Integer.parseInt(str[1]);
         long[] sumArr = new long[arr.length];
-        sumArr[0] = arr[0];
-        for(int i=1;i<arr.length;i++){
-            sumArr[i]= sumArr[i-1] + arr[i];
+        sumArr[0] = arr[0] % m;
+        for (int i = 1; i < arr.length; i++) {
+            sumArr[i] = sumArr[i - 1] + arr[i];
+            sumArr[i] %= m;
         }
 
-        long count = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] % m == 0){
-                count++;
+        long result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (sumArr[i] == 0) {
+                result++;
             }
         }
 
-        long continueCount = 0;
-        for(int i=0;i<sumArr.length;i++){
-            if(sumArr[i] % m == 0){
-                continueCount++;
-            }
+        Map<Long, Long> map = Arrays.stream(sumArr)
+            .boxed()
+            .collect(Collectors.groupingBy(
+                v -> v,
+                Collectors.counting()
+            ));
+
+        for (long value : map.values()) {
+            result += value * (value - 1) / 2;
         }
 
-        bw.write(count + (continueCount * (continueCount + 1)) / 2 + "");
+        bw.write(result + "");
         bw.flush();
     }
 }
