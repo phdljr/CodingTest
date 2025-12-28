6a8b94c72c5a2fa90793d851886d87b4520b2cca
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-29 03:07:51 +0900

    [Bronze III] Title: 웰컴 키트, Time: 136 ms, Memory: 16432 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =  Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] str = br.readLine().split(" ");
+        int t =  Integer.parseInt(str[0]);
+        int p =  Integer.parseInt(str[1]);
+
+        int r1 = 0;
+        for(int num: arr){
+            r1 += num/t + (num%t == 0 ? 0 : 1);
+        }
+
+        int r2 = n / p;
+        int r3 = n % p;
+
+        bw.write(r1+"\n" + r2 + " " + r3);
+        bw.flush();
+    }
+}
