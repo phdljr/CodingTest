c98f92d401bc5bf6d05e3f1f9c8a4e46a9cde21f
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-29 01:36:19 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14300 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        char[] arr =  br.readLine().toCharArray();
+
+        int result = 0;
+        for(int i=0;i<arr.length;i++){
+            int temp = arr[i] - 'a' + 1;
+            for(int j=0;j<i;j++){
+                temp *= 31 % 1234567891;
+            }
+            result += temp;
+        }
+
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
