2d880f73fd44d9afd570bb31cf18916c49827d11
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2025-12-29 01:43:02 +0900

    [Bronze I] Title: 팰린드롬수, Time: 96 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder result = new StringBuilder();
+        while (true) {
+            String str = br.readLine();
+            if(str.equals("0")) {
+                break;
+            }
+
+            if(str.contentEquals(new StringBuilder(str).reverse())) {
+                result.append("yes");
+            } else{
+                result.append("no");
+            }
+
+            result.append("\n");
+        }
+
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+
