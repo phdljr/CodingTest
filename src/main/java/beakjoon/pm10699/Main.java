dd3b961c4df2fdae37594143208341507ca859a9
phdljr <phdljr@naver.com>
2026-01-21 22:00:31 +0900

    add

---
+++ src/main/java/beakjoon/pm10699/Main.java
@@ -0,0 +1,29 @@
+package beakjoon.pm10699;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        LocalDate now = LocalDate.now();
+
+        bw.write(now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
