9cbbcb74b8773f5a7d43273a7acb20ac1ce7c608
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2026-04-01 22:25:12 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 240 ms, Memory: 19260 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+class Matrix{
+	int r;
+	int c;
+	
+	Matrix(int r, int c){
+		this.r = r;
+		this.c = c;
+	}
+}
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		Matrix[] m = new Matrix[n+1];
+		for(int i=1;i<=n;i++) {
+			String[] str = br.readLine().split(" ");
+			int r = Integer.parseInt(str[0]);
+			int c = Integer.parseInt(str[1]);
+			m[i] = new Matrix(r, c);
+		}
+		
+		// dp[i][j]: i부터 j까지의 최소 연산 횟수
+		int[][] dp = new int[n+1][n+1];
+		
+		for(int d=1;d<=n;d++) { // 행렬 간의 간격
+			for(int i=1;i+d<=n;i++) { // 시작 행렬
+				int j = i + d; // 끝 행렬
+				dp[i][j] = Integer.MAX_VALUE;
+				
+				for(int k=i;k<j;k++) { // 분할 지점
+					int cost = dp[i][k] + dp[k+1][j] + m[i].r * m[k].c * m[j].c;
+					dp[i][j] = Math.min(dp[i][j], cost);
+				}
+			}
+		}
+		
+		bw.write(dp[1][n] + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
