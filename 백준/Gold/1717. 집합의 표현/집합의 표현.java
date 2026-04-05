09e5dac095cf1e1543b2f5a4772882990b41f90d
Lee Jongryeol <68233342+phdljr@users.noreply.github.com>
2026-04-05 21:58:31 +0900

    [Gold V] Title: 집합의 표현, Time: 760 ms, Memory: 63160 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder result = new StringBuilder();
+
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        int[] p = new int[n+1];
+        for(int i=1;i<=n;i++){
+            p[i] = i;
+        }
+
+        for(int i=0;i<m;i++){
+            str = br.readLine().split(" ");
+            int c = Integer.parseInt(str[0]);
+            int a = Integer.parseInt(str[1]);
+            int b = Integer.parseInt(str[2]);
+
+            if(c == 0){
+                union(p, a, b);
+            } else{
+                int pa = find(p, a);
+                int pb = find(p, b);
+                if(pa == pb){
+                    result.append("YES\n");
+                }
+                else{
+                    result.append("NO\n");
+                }
+            }
+        }
+
+        bw.write(result.toString().trim());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static void union(int[] p, int a, int b) {
+        int aRoot = find(p, a);
+        int bRoot = find(p, b);
+
+        if(aRoot != bRoot){
+            if(aRoot < bRoot){ // 숫자가 작은 쪽을 부모로
+                p[bRoot] = aRoot;
+            } else{
+                p[aRoot] = bRoot;
+            }
+        }
+    }
+
+    private static int find(int[] p, int a) {
+        if(p[a] == a){
+            return a;
+        } else{
+            return p[a] = find(p, p[a]); // 경로 압축
+        }
+    }
+}
