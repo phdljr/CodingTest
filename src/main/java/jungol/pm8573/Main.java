fc0f4f6867437b5220191cab35efe3a982c1ca23
phdljr <phdljr@naver.com>
2026-05-12 22:12:33 +0900

    add

---
+++ src/main/java/jungol/pm8573/Main.java
@@ -0,0 +1,31 @@
+package jungol.pm8573;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =  Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        String result = "";
+        if (n % 2 == 1) {
+            result += arr[n / 2];
+        } else {
+            result += arr[n / 2 - 1] + " " + arr[n / 2];
+        }
+
+        bw.write(result);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
