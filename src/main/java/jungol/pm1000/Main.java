ee1dfff82121fef59a659591111d2a1789d4d8d7
phdljr <phdljr@naver.com>
2026-05-16 22:54:14 +0900

    add

---
+++ src/main/java/jungol/pm1000/Main.java
@@ -0,0 +1,22 @@
+package jungol.pm1000;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        bw.write(arr[0] + arr[1] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
